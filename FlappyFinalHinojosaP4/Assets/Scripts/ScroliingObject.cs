102deebef055820511327eb14156aa6f7479ed6d
BronzePotato <BronzeVulture101@gmail.com>
2023-01-23 11:18:12 -0800

    Done

--- FlappyFinalHinojosaP4/Assets/Scripts/ScroliingObject.cs
+++ FlappyFinalHinojosaP4/Assets/Scripts/ScroliingObject.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ScroliingObject : MonoBehaviour
+public class ScrollingObject : MonoBehaviour
 {
     private Rigidbody2D rb2d;
     // Start is called before the first frame update
